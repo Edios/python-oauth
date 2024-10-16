53bcefcc1e9f388e8082ddd4db7f0578dcbf48d8
Miroslaw <miroslaw.lazarewicz@gmail.com>
2024-10-16 21:55:37 +0200

    Initial version of data server

--- main.py
+++ main.py
@@ -5,9 +5,17 @@ from auth_server import Client, AuthServer
 from custom_exceptions import InvalidToken, AccessTokenExpired
 
 
+class DataServer:
+    auth_server: AuthServer
+
+    def get_user_info(self, access_token)->:
+        pass
+
+
 def main(user, password):
     client = Client(user, password)
     auth_server = AuthServer()
+    data_server = DataServer(auth_server)
     access_token, refresh_token = auth_server.request_token(client)
 
     if not auth_server.is_access_token_valid(access_token):
@@ -16,6 +24,8 @@ def main(user, password):
         raise AccessTokenExpired("Access token expired. Use refresh token to renew it.")
 
     access_token = auth_server.refresh_token(refresh_token)
+
+    data_server.get_user_info(access_token)
 
 
 if __name__ == '__main__':
